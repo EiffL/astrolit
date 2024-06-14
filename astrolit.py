11c3788ca1665536fff725990e4e2fb5b6b70e69
Leopoldo <lsarra@flatironinstitute.org>
2024-06-14 18:47:05 -0400

    improved interface

--- astrolit.py
+++ astrolit.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 import os
 import streamlit as st
-import streamlit_nested_layout
+import streamlit_nested_layout  # enable nested cols
 from sparcl.client import SparclClient
 
 # from debug import compare_snapshots, init_tracking_object
@@ -53,13 +53,12 @@ header_cols = st.columns((1))
 header_cols[0].title("Welcome to AstroCLIP Galaxy Finder")
 header_cols[0].markdown(
     """
-    Created by [Polymathic](https://polymathic-ai.org).
-    
-    Starting from [George Stein](https://georgestein.github.io/)'s version.
+    Check out the retrieval abilities of our new multimodal model with this interactive web app! 
+    Read our [blog post](https://polymathic-ai.org/blog/astroclip_update/) to learn more.
     """
 )
 
-display_method = header_cols[-1].button("Interested in learning how this works?")
+display_method = header_cols[-1].button("About")
 
 
 ###########################################################
@@ -100,19 +99,25 @@ with st.spinner(text="Loading embedding dataset..."):
 
 # Initialize default
 if "ra" not in st.session_state:
-    st.session_state["ra"] = "217.09"
+    st.session_state["ra"] = "190.045"
 if "dec" not in st.session_state:
-    st.session_state["dec"] = "35.41"
+    st.session_state["dec"] = "62.883"
+
+sidebar = st.sidebar
+sidebar.header("Search a galaxy")
 
 
 def describe_method():
-    st.button("Back to Galaxy Finder")
+    st.button("Back")
 
     st.markdown(
         """
-        ## TODO: Add AstroCLIP paper reference and comments.
-        ### [AstroCLIP](https://arxiv.org/abs/2310.03024)
-        ### A bit about the method: 
+        ## AstroCLIP: A Cross-Modal Foundation Model for Galaxies
+        - [Paper](https://arxiv.org/abs/2310.03024)
+        - [Blog](https://polymathic-ai.org/blog/astroclip_update/)
+        - [Code](https://github.com/PolymathicAI/AstroCLIP)
+        
+        A bit about the method: 
         - The similarity of two images is quite easy to judge by eye - but writing an algorithm to do the same is not as easy as one might think! This is because as hunans we can easily identify and understand what object is in the image.                     
         - A machine is different - it simply looks individual pixezl values. Yet two images that to us have very similar properties and appearences will likely have vastly different pixel values. For example, imagine rotating a galaxy image by 90 degrees. It it obviously still the same galaxy, but the pixel values have completeley changed.                                       
         - So the first step is to teach a computer to understand what is actually in the image on a deeper level than just looking at pixel values. Unfortunately we do not have any information alongside the image specifying what type of galaxy is actually in it - so where do we start?                                                                                                   
@@ -125,38 +130,30 @@ def describe_method():
         Dataset:
         
         - We used galaxy images from [DECaLS DR9](https://www.legacysurvey.org/), randomly sampling 3.5 million galaxies to train the machine learning model. We then apply it on every galaxy in the dataset, about 42 million galaxies with z-band magnitude < 20, so most bright things in the sky should be included, with very dim and small objects likely missing - more to come soon!                                                   
-        - The models were trained using images of size 96 pixels by 96 pixels centered on the galaxy. So features outside of this central region are not used to calculate the similarity, but are sometimes nice to look at                                                                                                         
-        Please note this project is ongoing, and results will continue to be updated and improved.           
-        Created by [George Stein](https://georgestein.github.io/)                      
+        - The models were trained using images of size 96 pixels by 96 pixels centered on the galaxy. So features outside of this central region are not used to calculate the similarity, but are sometimes nice to look at                                                                                                                
+        Forked from [George Stein](https://georgestein.github.io/)  
+        Created by [Polymathic AI](https://polymathic-ai.org/)
+        
+        _**Advancing Science through Multi‑Disciplinary AI**_:
+        _We usher in a new class of machine learning for scientific data, building models that can leverage shared concepts across disciplines. We aim to develop, train, and release such foundation models for use by researchers worldwide._
          """
     )
     st.button(
-        "Back to Galaxy Finder", key="galaxies"
+        "Back", key="galaxies"
     )  # will change state and hence trigger rerun and hence reset should_tell_me_more
 
 
 def galaxy_search():
-
-    with st.sidebar.expander("Instructions"):
-        st.markdown(
-            """
-                **Enter the coordinates of your favourite galaxy and we'll search for the most similar looking ones in the universe!**
-                
-                Click the 'search random galaxy' button, or try finding a cool galaxy at [legacysurvey.org](https://www.legacysurvey.org/viewer)
-                - Use the south survey (select the <Legacy Surveys DR9-south images> option). Currently not all galaxies are included, but most bright ones should be.
-                """
-        )
-
-    st.sidebar.markdown("### Search a galaxy")
     ra_unit_formats = "degrees or HH:MM:SS"
     dec_unit_formats = "degrees or DD:MM:SS"
 
-    ra_search = st.sidebar.text_input(
+    c1, c2 = sidebar.columns(2)
+    ra_search = c1.text_input(
         "RA",
         key="ra",
         help="Right Ascension of query galaxy ({:s})".format(ra_unit_formats),
     )
-    dec_search = st.sidebar.text_input(
+    dec_search = c2.text_input(
         "Dec",
         key="dec",
         help="Declination of query galaxy ({:s})".format(dec_unit_formats),
@@ -166,42 +163,79 @@ def galaxy_search():
     )
 
     # modality
-    source_modality = st.sidebar.radio(
+    source_modality = sidebar.radio(
         "Search from",
         [ModalityEnum.IMAGE, ModalityEnum.SPECTRUM, ModalityEnum.BOTH],
         help="Search galaxies similar to either the image embedding, the associated spectrum embedding, or the average of both.",
     )
-    target_modality = st.sidebar.radio(
+    target_modality = sidebar.radio(
         "Compare with",
         [ModalityEnum.IMAGE, ModalityEnum.SPECTRUM, ModalityEnum.BOTH],
         help="Look for similar galaxies using either the image embedding, the associated spectrum embedding, or the average of both.",
     )
 
-
-
     # dummy
-    search = st.sidebar.button("Search")
 
-    def pick_random():
+    def pick_suggested_random():
         input_ra, input_dec = SUGGESTED_GALAXIES[
             np.random.choice(len(SUGGESTED_GALAXIES))
         ]
         st.session_state.ra = f"{input_ra:2.2f}"
         st.session_state.dec = f"{input_dec:2.2f}"
 
-    choose_random = st.sidebar.button("Pick random galaxy", on_click=pick_random)
+    def pick_random():
+        idx = np.random.choice(len(dataset))
+        input_ra = dataset["ra"][idx]
+        input_dec = dataset["dec"][idx]
+        st.session_state.ra = f"{input_ra:2.2f}"
+        st.session_state.dec = f"{input_dec:2.2f}"
+
+    first = sidebar.container()
+    second, third = sidebar.columns([6, 4])
+    search = first.button(":mag:  Search", use_container_width=True)
+    choose_suggested_random = second.button(
+        "Random (bright)",
+        on_click=pick_suggested_random,
+        help="Pick a random galaxy among a few bright galaxies we selected from the database",
+        use_container_width=True,
+    )
+    choose_random = third.button(
+        "Random",
+        on_click=pick_random,
+        help="Pick a random galaxy in the entire dataset",
+        use_container_width=True,
+    )
 
     num_nearest_vals = [i**2 for i in range(3, 6)]
-    nnearest = st.sidebar.select_slider(
+    nnearest = sidebar.select_slider(
         "Number of similar galaxies to display", num_nearest_vals
     )
 
     with st.spinner("Loading images and spectra from Legacy Survey server..."):
-        show_results(input_ra, input_dec, dataset, nnearest, source_modality, target_modality)
+        show_results(
+            input_ra, input_dec, dataset, nnearest, source_modality, target_modality
+        )
+
+    with sidebar.expander(":question: Instructions"):
+        st.markdown(
+            """
+                **Enter the coordinates of your favourite galaxy and we'll search for the most similar looking ones in the universe!**
+                
+                Click the 'search random galaxy' button, or try finding a cool galaxy at [legacysurvey.org](https://www.legacysurvey.org/viewer)
+                Currently not all galaxies are included, but most bright ones should be.
+                """
+        )
+    sidebar.markdown("------")
+    sidebar.markdown(
+        "[Polymathic AI](https://polymathic-ai.org/)",
+        help="Advancing Science through Multi‑Disciplinary AI: we usher in a new class of machine learning for scientific data, building models that can leverage shared concepts across disciplines. We aim to develop, train, and release such foundation models for use by researchers worldwide.",
+    )
 
 
 @st.experimental_fragment
-def show_results(input_ra, input_dec, dataset, nnearest, source_modality, target_modality):
+def show_results(
+    input_ra, input_dec, dataset, nnearest, source_modality, target_modality
+):
     # Search object at location
     input_object = search_catalogue(input_ra, input_dec, dataset)
 
@@ -236,9 +270,7 @@ def show_results(input_ra, input_dec, dataset, nnearest, source_modality, target
     else:
         print("Invalid modality option")
 
-    result_idx = similarity_search(
-        query[query_index], target, nnearest=nnearest + 1
-    )
+    result_idx = similarity_search(query[query_index], target, nnearest=nnearest + 1)
     result_images = []
     for targetid in clip_targetid[result_idx["index"]]:
         idx = np.argwhere(clip_targetid == targetid)[0]
@@ -259,12 +291,12 @@ def show_results(input_ra, input_dec, dataset, nnearest, source_modality, target
         input_object["ra"], input_object["dec"]
     )
 
-    page_cols = st.columns([3,7])
+    page_cols = st.columns([3, 7])
 
     # Container #0 (left): query galaxy
     query_container = page_cols[0].container(border=False)
     query_container.subheader(lab)
-    
+
     # Always show both
     image_raw_url = get_image_url_from_coordinates(ra=ra, dec=dec)
     query_container.image(
@@ -302,9 +334,9 @@ def show_results(input_ra, input_dec, dataset, nnearest, source_modality, target
             image_url = result_images[iimg]
             if ncolumns > 5:
                 lab = None
-                
+
             current_container = result_cols[icol].container(border=True)
-            
+
             current_image, current_spectrum = current_container.columns(2)
             # add image to grid
             current_image.image(
@@ -316,14 +348,17 @@ def show_results(input_ra, input_dec, dataset, nnearest, source_modality, target
             spectrum = result_spectra[iimg]
             fig = plt.figure()
             plt.plot(spectrum.squeeze()[::20])
-            current_spectrum.pyplot(fig, 
-                                    #  use_container_width=True
-                                    )
+            current_spectrum.pyplot(
+                fig,
+                #  use_container_width=True
+            )
 
             # current_container.markdown(f\n")
-            current_container.progress(value=float(result_idx['score'][iimg]), text=f"Similarity={result_idx['score'][iimg]:.4f}")
+            current_container.progress(
+                value=float(result_idx["score"][iimg]),
+                text=f"Similarity={result_idx['score'][iimg]:.4f}",
+            )
             iimg += 1
-            
 
 
 if display_method:
